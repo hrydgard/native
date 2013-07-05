dc1abc5656f3b3c28648e53e38b72936ebdd7f78
Freetos <Gmasterzx@gmail.com>
2013-07-05 18:11:08 -0400

    Update NativeRenderer.java

--- android/src/com/henrikrydgard/libnative/NativeRenderer.java
+++ android/src/com/henrikrydgard/libnative/NativeRenderer.java
@@ -3,13 +3,18 @@ package com.henrikrydgard.libnative;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
 
 
 public class NativeRenderer implements GLSurfaceView.Renderer {
 	private static String TAG = "NativeRenderer";
-	NativeActivity mActivity;
+	public static int brightness;
+	
+	static NativeActivity mActivity;
 	
 	NativeRenderer(NativeActivity act) {
 		mActivity = act;
@@ -31,13 +36,24 @@ public class NativeRenderer implements GLSurfaceView.Renderer {
 		displayResize(width, height);
 	}
 	
+	public static void getCurrentBrightness() {
+		WindowManager.LayoutParams settings = mActivity.getWindow().getAttributes();
+		brightness = Math.round(settings.screenBrightness*100);
+	}
+	
+	public static void setCurrentBrightness(int value) {
+		WindowManager.LayoutParams settings = mActivity.getWindow().getAttributes();
+        settings.screenBrightness = (float)value/100;
+        mActivity.getWindow().setAttributes(settings);
+	}
 	
 	// NATIVE METHODS
 
 	// Note: This also means "device lost" and you should reload
 	// all buffered objects. 
 	public native void displayInit(); 
-	
+	public static native int displayGetBrightness();
+	public static native void displaySetBrightness(int value);
 	public native void displayResize(int w, int h);
 	public native void displayRender();
 	
